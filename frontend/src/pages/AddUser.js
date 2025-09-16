034bd3c57d1a8fa37b786be538bf0f84f171f5d0
Chandra <121284719+chandrasekhargi@users.noreply.github.com>
2025-09-16 15:22:41 +0530

    Update AddUser.js

--- frontend/src/pages/AddUser.js
+++ frontend/src/pages/AddUser.js
@@ -24,7 +24,7 @@ function AddUser() {
         geo: { lat: form.lat, lng: form.lng }
       }
     };
-    axios.post("http://localhost:5000/api/users", payload).then(() => navigate("https://user-management-assignment-front-4.onrender.com"));
+    axios.post("https://user-management-assignment-6.onrender.com/api/users", payload).then(() => navigate("/"));
   };
 
   return (
